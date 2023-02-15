e68fe1badfdf4ca1a036353bd5387aa973aeae16
Piotr Mikulowski <mikuspiotr123@gmail.com>
2023-02-15 13:55:31 +0000

    Add header and source files representing new ObstacleField cost function

---
+++ base_local_planner/include/base_local_planner/obstacle_rettractive_field.h
@@ -0,0 +1,38 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2023
+ *  All rights reserved.
+ *
+ * Author: Piotr Mikulowski @rureverek
+ *********************************************************************/
+
+#ifndef OBSTACLE_RETT_FIELD_H
+#define OBSTACLE_RETT_FIELD_H
+
+#include <base_local_planner/trajectory_cost_function.h>
+
+#include <costmap_2d/costmap_2d.h>
+#include <base_local_planner/map_grid.h>
+
+namespace base_local_planner {
+
+class ObstacleFieldCostFunction: public base_local_planner::TrajectoryCostFunction {
+public:
+  ObstacleFieldCostFunction(costmap_2d::Costmap2D* costmap);
+
+  ~ObstacleFieldCostFunction() {}
+
+  double scoreTrajectory(Trajectory &traj);
+
+  unsigned char getCellCosts(unsigned int cx, unsigned int cy);
+
+private:
+  std::vector<geometry_msgs::PoseStamped> target_poses_;
+  costmap_2d::Costmap2D* costmap_;
+
+};
+
+} /* namespace base_local_planner */
+#endif /* OBSTACLE_RETT_FIELD_H */
---
+++ base_local_planner/src/obstacle_rettractive_field.cpp
@@ -0,0 +1,49 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2023
+ *  All rights reserved.
+ *
+ * Author: Piotr Mikulowski @rureverek
+ *********************************************************************/
+
+#include <base_local_planner/obstacle_rettractive_field.h>
+
+namespace base_local_planner {
+
+ObstacleFieldCostFunction::ObstacleFieldCostFunction(costmap_2d::Costmap2D* costmap) : 
+    costmap_(costmap),
+    map_(costmap->getSizeInCellsX(), costmap->getSizeInCellsY()) {}
+
+unsigned char ObstacleFieldCostFunction::getCellCosts(unsigned int px, unsigned int py) {
+
+  unsigned char cell_cost = costmap_->getCost(px,py);
+  return cell_cost;
+}
+
+double ObstacleFieldCostFunction::scoreTrajectory(Trajectory &traj) {
+  double cost = 0.0;
+  double px, py, pth;
+  unsigned int cell_x, cell_y;
+  unsigned char cell_cost;
+
+  for (unsigned int i = 0; i < traj.getPointsSize(); ++i) {
+    traj.getPoint(i, px, py, pth);
+
+    //we won't allow trajectories that go off the map... shouldn't happen that often anyways
+    if ( ! costmap_->worldToMap(px, py, cell_x, cell_y)) {
+      //we're off the map
+      ROS_WARN("Off Map %f, %f", px, py);
+      return -4.0;
+    }
+
+    cell_cost = getCellCosts(cell_x, cell_y);
+
+    cost += cell_cost;
+
+  }
+  return cost;
+}
+
+} /* namespace base_local_planner */
